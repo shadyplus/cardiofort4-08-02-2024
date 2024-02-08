9b7ec53b1146609ba2ce24cdd4c8b59915bd32a9
shadyplus <86576859+shadyplus@users.noreply.github.com>
2024-02-08 15:22:00 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,1 @@
+var landslider;$(document).ready(function(){$(".slider").bxSlider({pager:true,controls:true,auto:false,infiniteLoop:false});$(".toform").click(function(){$("html, body").animate({scrollTop:$("form").offset().top-30},1000);return false})});
